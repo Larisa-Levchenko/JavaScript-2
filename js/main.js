e5888107b32b197c423b6c6294c25a15b9eb851c
Larisa-Levchenko <larisa.levchenko.98@mail.ru>
2020-08-07 12:39:19 +0700

    Отправка ДЗ

---
+++ js/main.js
@@ -0,0 +1,10 @@
+let num = 266219;
+let str = String(num);
+
+let product = str[0]*str[1]*str[2]*str[3]*str[4]*str[5];
+console.log(product);
+
+let power = String(product ** 3);
+console.log(power.substr(0,2));
+
+
