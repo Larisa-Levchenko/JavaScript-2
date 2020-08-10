fde2ef169cef57e68f65b998b1044f5492766bab
Larisa-Levchenko <larisa.levchenko.98@mail.ru>
2020-08-10 20:25:07 +0700

    feat in js/main.js

--- js/main.js
+++ js/main.js
@@ -1,13 +1,29 @@
-let num = 266219;
-let str = String(num);
+let lang = prompt('Введите "ru" или "en"');
 
-let product = 1;
-for(let i=0; i<str.length; i++) {
-    product*=str[i];
+if (lang ==='ru'){
+    console.log('понедельник, вторник, среда, четверг, пятница, суббота, воскресенье');
+}else if(lang ==='en'){
+    console.log('Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday');
 }
-console.log(product);
 
-let power = String(product ** 3);
-console.log(power.substring(0,2));
+switch(lang){
+    case 'ru':
+        console.log('понедельник, вторник, среда, четверг, пятница, суббота, воскресенье');
+        break;
+    case 'en':
+        console.log('Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday');
+        break;
+}
+
+let days = [
+    ['понедельник, вторник, среда, четверг, пятница, суббота, воскресенье'],
+    ['Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday']
+];
+let res = lang==='ru' ? console.log('понедельник, вторник, среда, четверг, пятница, суббота, воскресенье')
+:console.log('Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday');
 
 
+let namePerson = prompt('Введите имя');
+
+let result = namePerson==='Артем' ? console.log('директор') 
+: namePerson ==='Максим' ? console.log('преподаватель') : console.log('студент');
