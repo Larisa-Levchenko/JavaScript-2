220e32ab4a3fa300383a21027f8d01bb6ccbb5ee
Larisa-Levchenko <larisa.levchenko.98@mail.ru>
2020-08-15 16:43:45 +0700

    fix lesson07 js/main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 'use strict';
 
-
 let div = document.getElementById('div');
 let week = ['понедельник', 'вторник','среда','четверг','пятница','суббота','воскресенье'];
 
@@ -9,13 +8,13 @@ let liFirst = document.createElement('p');
 
 
 for (let i = 0; i < week.length; i++) {
-    if (week[i] === dateWeek) {
-        div.innerHTML = div.innerHTML + week[i].bold() + '<br>';
+    if (week[i] === dateWeek && (week[i] === 'суббота' || week[i] === 'воскресенье')) {
+        div.innerHTML = div.innerHTML + week[i].bold().italics() + '<br>';
     } else if (week[i] === 'суббота' || week[i] === 'воскресенье') {
         div.innerHTML = div.innerHTML + week[i].italics() + '<br>';
+    } else if (week[i] === dateWeek) {
+        div.innerHTML = div.innerHTML + week[i].bold() + '<br>';
     } else {
         div.innerHTML = div.innerHTML + week[i] + '<br>';
-        
-    }  
+    }
 }
-
