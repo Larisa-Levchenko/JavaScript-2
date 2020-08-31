e642514f150a99f651042c26942e84dbaf41adbb
Larisa-Levchenko <larisa.levchenko.98@mail.ru>
2020-08-31 20:42:42 +0700

    feat lesson18 in js

--- js/main.js
+++ js/main.js
@@ -1,18 +1,71 @@
 'use strict';
 
-const text = document.querySelector('.text');
-const btn = document.querySelector('.btn');
-const body = document.querySelector('body');
+const date = new Date();
 
-const Random = function(){
-    let num = Math.floor(Math.random() * Math.floor(16777215));
-    num = '#'+num.toString(16);
-    return num;
-};
+let hour = date.getHours();
+let minute = date.getMinutes();
+let sec = date.getSeconds();
 
-btn.addEventListener('click', function(){
-    let color = Random();
-    text.textContent=color;
-    body.style.backgroundColor = color;
-    btn.style.color=color;
+let dateWeek = date.toLocaleString('ru', {
+     weekday: 'long'
 });
+function getZero(n) {
+    if (n < 10) {
+        n = '0' + n;
+    }
+    return n;
+}
+
+function timeDay(){
+    if (hour > 6 && hour < 12) {
+        return 'Доброе утро';
+    } else if (hour > 12 && hour <18) {
+        return 'Добрый день';
+    } else if (hour > 18) {
+        return 'Добрый вечер';
+    } else {
+        return 'Доброй ночи';
+    }
+}
+
+function getTime(){
+    if(hour<12){
+        return `${getZero(hour)}:${getZero(minute)}:${getZero(sec)} AM`;
+    }else{
+        return `${getZero(hour-12)}:${getZero(minute)}:${getZero(sec)} PM`;
+    }
+}
+function getTimeREmaining() {
+    let dateStop = new Date('1 January 2021').getTime(),
+        dateNow = new Date().getTime(),
+        timeRemaining = (dateStop - dateNow) / 1000/60/60/24;
+        return Math.ceil(timeRemaining);
+}
+
+function getDay(n){
+    if (n % 10 === 1 && Math.trunc(n / 10) !== 1) {
+        return `${n} день`;
+    } else {
+        if (n % 10 > 1 && n % 10 < 5 && Math.trunc(n / 10) !== 1) {
+            return `${n} дня`;
+        } else {
+            return `${n} дней`;
+        }
+    }
+}
+
+let greeting = document.createElement('p');
+greeting.textContent=timeDay();
+document.body.append(greeting);
+
+let today = document.createElement('p');
+today.textContent = `Сегодня: ${dateWeek}`;
+document.body.append(today);
+
+let time = document.createElement('p');
+time.textContent = getTime();
+document.body.append(time);
+
+let newYear = document.createElement('p');
+newYear.textContent = `До нового года осталось ${getDay(getTimeREmaining())}`;
+document.body.append(newYear);
